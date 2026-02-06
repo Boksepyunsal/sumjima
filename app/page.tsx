cdab21a3a8a91b9789c54484eff44a46bf8a5413
jaeholee <kojotourist@gmail.com>
2026-02-06 15:41:38 +0900

    Test: Should fail due to Airbnb style violation

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ import { Card, CardContent } from '@/components/ui/card';
 import { ArrowRight, PenTool, MessageCircle, Wallet } from 'lucide-react'; 
 
 export default function LandingPage() {
+  var airbnbViolation = 'This should fail commit';
   return (
     <div className="flex min-h-screen flex-col">
       <Header />
